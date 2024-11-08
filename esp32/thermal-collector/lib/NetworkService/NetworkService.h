2c3bec0c9fc888599ce0e078d3b91332175e4ad9
y.blonski <y.blonski@freeplay.io>
2024-11-08 16:38:19 +0300

    feat(wifi-setup): add a process of configuring wifi using esp32 access point

--- esp32/thermal-collector/lib/NetworkService/NetworkService.h
+++ esp32/thermal-collector/lib/NetworkService/NetworkService.h
@@ -1,11 +1,23 @@
 #ifndef NETWORK_SERVICE_H
 #define NETWORK_SERVICE_H
 
+#include "ESPAsyncWebServer.h"
+#include "Arduino.h"
+
 class NetworkService
 {
 public:
-    void ConnectToWifi(const char *ssid, const char *password);
+    void WaitForConfigurationAndConnect();
+    void MakeInitialConfigurationWifiAP();
     unsigned char *GetMacAddress(bool printToSerial);
+    
+private:
+    unsigned long currentTime_ = millis();
+    unsigned long previousTime_ = 0; 
+    // Define timeout time in milliseconds (example: 2000ms = 2s)
+    const long timeoutTime_ = 2000;
+    AsyncWebServer server_ = AsyncWebServer(80);
+    bool connectedToTargetWifi_;
 };
 
 #endif
